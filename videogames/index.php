6ceff41259fffdb04d52bd016356273ba947eae8
MFuentes1992 <markfuentes2991@gmail.com>
2020-06-03 17:53:44 -0500

    Logo link change to pint dashboard for all dashboard sections

--- videogames/index.php
+++ videogames/index.php
@@ -65,7 +65,7 @@
           </button>
         </div>
         <div class="col-md-6" id="logoContainer">
-          <a href="<?=$url;?>" style="margin-left: 45%;"><img class="img_logo" src="<?=$url; ?>img/horizontal_alt.png" alt="cirkuits logo" width="240" height="100"/></a>
+          <a href="<?=$url;?>dashboard" style="margin-left: 45%;"><img class="img_logo" src="<?=$url; ?>img/horizontal_alt.png" alt="cirkuits logo" width="240" height="100"/></a>
         </div>
         <div class="col-md-3" id="avatarContainer">
           <div class="line" style="margin-top:20px;">
