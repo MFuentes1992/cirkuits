620768e26d6065af17b4c66ff5bb58b7a00fe740
MFuentes1992 <markfuentes2991@gmail.com>
2020-06-03 20:48:43 -0500

    New Section - Leaderboard (Mockup)

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -1,158 +1,244 @@
 <?php
-include_once("../util/utilities.php");
-require_once("../util/funciones.php");
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-session_start();
-if(isset($_SESSION["user"]))
-{
-  if(isset($_SESSION["user"]["estatus_usuario"]))
+  include_once("../util/utilities.php");
+  include_once("../util/funciones.php");
+  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
+  session_start();
+  if(isset($_SESSION["user"]))
   {
-    if($_SESSION["user"]["estatus_usuario"] == 1)
+    if(isset($_SESSION["user"]["estatus_usuario"]))
     {
-      header("Location:".$url."payment");
+      if($_SESSION["user"]["estatus_usuario"] == 1)
+      {
+        /*Disabeling user payment*/
+        header("Location:".$url."payment");        
+      }
+      else if($_SESSION["user"]["estatus_usuario"] == 2){
+        $payment = 2;
+        $videogameName = "";
+        if(isset($_GET["game"])){
+          $strQueryVideogame = sprintf("SELECT nombre from cat_videogames WHERE id_videogame = %s",
+          GetSQLValueString($conexion, $_GET["game"], 'int'));
+          $rawVideogame = mysqli_query($conexion, $strQueryVideogame)or die(mysqli_error($conexion));
+          $resultVideogame = mysqli_fetch_assoc($rawVideogame);
+          $videogameName = $resultVideogame["nombre"];
+        }else{
+          header("location:".$url."videogames");
+        }
+      }
+      else {
+        header("location:".$url."signin");
+      }
     }
-    else if($_SESSION["user"]["estatus_usuario"] == 2){
-
-      $videogame = isset($_GET["id"]) ? $_GET["id"] : 1;
-
-    }
-    else {
-      header("location:".$url."singin");
-    }
+  }else {
+    header("location:".$url."signin");
   }
-}else {
-  header("location:".$url."singin");
-}
  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Standardised web app manifest -->
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="<?=$url;?>css/bootstrap-4.3.1/dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="<?=$url;?>css/cirkuits.css" />
+    <link rel="stylesheet" href="<?=$url;?>css/master.css" />
+    <link rel="stylesheet" href="<?=$url;?>css/jquery-ui.css" />
+    <link rel="stylesheet" href="<?=$url;?>css/fontawesome-free-5.8.1-web/css/all.css">
+    <link rel="stylesheet" href="<?=$url;?>css/validationEngine.jquery.css" />
+    <link rel="stylesheet" href="<?=$url;?>/js/swiper-5.3.6/package/css/swiper.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Coiny" rel="stylesheet">
+    <script src="<?=$url;?>js/jquery-1.12.3.min.js"></script>
+    <script src="<?=$url;?>js/dist/bootstrap.min.js"></script>
+    <script src="<?=$url;?>js/jquery-ui.js"></script>
+    <script src="<?=$url;?>js/jquery.validationEngine-es.js"></script>
+    <script src="<?=$url;?>js/jquery.validationEngine.js"></script>
+    <script src="<?=$url;?>js/swiper-5.3.6/package/js/swiper.min.js"></script>
+</head>
+<body class="leaderboard-section-body">
+    <div class="pos-f-t">
+      <nav class="navbar sticky-top navbar-dark bg-dark">
+        <div class="col-md-1" id="toggle">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div class="col-md-6" id="logoContainer">
+          <a href="<?=$url;?>dashboard" style="margin-left: 45%;"><img class="img_logo" src="<?=$url; ?>img/horizontal_alt.png" alt="cirkuits logo" width="240" height="100"/></a>
+        </div>
+        <div class="col-md-3" id="avatarContainer">
+          <div class="line" style="margin-top:20px;">
+            <a class="nav-link" href="<?=$url;?>exit.php"><span class="badge badge-danger">Log out</span></a>    
+          </div>
+          <div class="line">
+            <a class="nav-link" href="<?=$url;?>profile"> <img src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="avatar.png" class="img img-rounded" width="64px" style="top:-10px" /> </a>
+          </div>
+        </div>
+      </nav>    
+      <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark" style="padding-left:1.5rem;">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link white" href="<?=$url;?>dashboard"><i class="fas fa-window-maximize"></i>&nbsp;Dashboard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link white" href="<?=$url;?>payment/"><i class="far fa-credit-card"></i>&nbsp;Payment and Subscription</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link white" href="<?=$url;?>videos/"><i class="fas fa-film"></i>&nbsp;Videos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link white" href="<?=$url;?>videogames/"><i class="fas fa-gamepad"></i>&nbsp;Videogames</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link white" href="<?=$url;?>materials/"><i class="far fa-file-pdf"></i>&nbsp;Materials</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link white" href="<?=$url;?>support/"><i class="far fa-comments"></i>&nbsp;support</a>
+            </li>
+            <li id="resavatar" class="hidden">              
+            </li>
+            <li id='reslogout' class="hidden">              
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  <div class="container-fluid leaderboard-section-container">
+    <h1 id="leaderboard-title">Leaderboard:&nbsp;<?=$videogameName?></h1>
+    <br>
+    <br>
+    <div class="leaderboard-container">
+        <div class="leaderboard-item">
+          <div class="item-picture">
+            <img src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="avatar.png" class="img img-rounded" width="128px">
+          </div>
+          <div class="item-data">
+            <div class="item-name">
+              <?= $_SESSION["user"]["nombre_usuario"] ?>
+            </div>
+            <div class="item-hscore">
+              100
+            </div>
+          </div>
+        </div>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-   <!-- Standardised web app manifest -->
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7" />
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-   <title>PROFILE</title>
-   <link rel="manifest" href="appmanifest.json" />
-   <link rel="stylesheet" href="<?=$url;?>css/bootstrap-4.3.1/dist/css/bootstrap.css" />
-   <link rel="stylesheet" href="<?=$url;?>css/cirkuits.css" />
-   <link rel="stylesheet" href="<?=$url;?>css/master.css" />
-   <link rel="stylesheet" href="<?=$url;?>css/jquery-ui.css" />
-   <link rel="stylesheet" href="<?=$url;?>css/fontawesome-free-5.8.1-web/css/all.css">
-   <link rel="stylesheet" href="<?=$url;?>css/validationEngine.jquery.css" />
-   <link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
-   <link href="https://fonts.googleapis.com/css?family=Coiny" rel="stylesheet">
-   <script src="<?=$url;?>js/jquery-1.12.3.min.js"></script>
-   <script src="<?=$url;?>js/dist/bootstrap.min.js"></script>
-   <script src="<?=$url;?>js/jquery-ui.js"></script>
-   <script src="<?=$url;?>js/jquery.validationEngine-es.js"></script>
-   <script src="<?=$url;?>js/jquery.validationEngine.js"></script>
+        <div class="leaderboard-item">
+          <div class="item-picture">
+            <img src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="avatar.png" class="img img-rounded" width="128px">
+          </div>
+          <div class="item-data">
+            <div class="item-name">
+              <?= $_SESSION["user"]["nombre_usuario"] ?>
+            </div>
+            <div class="item-hscore">
+              100
+            </div>
+          </div>
+        </div>
 
- </head>
- <body>
-   <nav class="navbar navbar-default navbar-fixed-top menu">
-     <div class="container-fluid">
-       <div class="navbar-header">
-         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#cirkuitsNavbar">
-           <span class="icon-bar"></span>
-           <span class="icon-bar"></span>
-           <span class="icon-bar"></span>
-         </button>
-         <a href="<?=$url;?>dashboard" class="navbar-brand"><img src="<?=$url;?>img/logo2.png" alt="Logo Cirkuits" class="img-navbar"/></a>
-       </div>
-       <div class="collapse navbar-collapse" id="cirkuitsNavbar">
-         <ul class="nav navbar-nav navbar-right">
-           <li><a href="<?=$url;?>dashboard"><strong>Dashboard</strong></a></li>
-           <li><a href="<?=$url;?>subscription"><strong>Subscription</strong></a></li>
-           <li><a href="<?=$url?>updatepayment/"><strong>Update payment</strong></a></li>
-           <li><a href="<?=$url;?>profile"><img src="<?=$url;?>img/avatars/person-flat.png" alt="avatar.png" class="img img-rounded" width="32px" style="top:-10px" /></a></li>
-           <li><a href="<?=$url;?>profile"><strong><?php echo $_SESSION["user"]["alter_usuario"] ?></strong></a></li>
-           <li><a href="<?=$url;?>exit.php"><span class="label label-danger">Log out</span></a></li>
-         </ul>
-       </div>
-     </div>
-   </nav>
-   <div class="container-fluid">
-     <div class="row">
-       <div class="contenido" id="tleaderboard">
-         <br>
-         <h3>Leaderboard</h3>
-         <br>
-         <?php if(mysqli_num_rows($result_hscore) > 0) {?>
-          <table width="100%" style="text-align: center;">
-           <thead style="background-color:#424242; color:#FFF;">
-             <th>
-               Rank
-             </th>
-             <th>
-               <span>Usuario</span>
-             </th>
-             <th>
-               <span>Score</span>
-             </th>
-           </thead>
-           <tbody>
-             <?php
-             $ranker = 1;
-              while($row = mysqli_fetch_assoc($result_hscore))
-              {
-                $query_get_user = sprintf("SELECT alter_usuario FROM usuarios WHERE id_usuario = %s",
-                GetSQLValueString($conexion, $row["id_usuario"], "int"));
-                $result_get_user = mysqli_query($conexion, $query_get_user)or die(mysqli_error($conexion));
-                $r = mysqli_num_rows($result_get_user) > 0 ? mysqli_fetch_assoc($result_get_user) : NULL;
-             ?>
-                <tr style="margin-bottom:5%;">
-                <td>
-                  <?=$ranker++;?>
-                </td>
-                <td>
-                  <?=$r["alter_usuario"]?>
-                </td>
-                <td>
-                  <?=$row["high_score"];?>
-                </td>
-              </tr>
-             <?php
-              }
-              ?>
-           </tbody>
-         </table>
-         <?php } else {?>
+        <div class="leaderboard-item">
+          <div class="item-picture">
+            <img src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="avatar.png" class="img img-rounded" width="128px">
+          </div>
+          <div class="item-data">
+            <div class="item-name">
+              <?= $_SESSION["user"]["nombre_usuario"] ?>
+            </div>
+            <div class="item-hscore">
+              100
+            </div>
+          </div>
+        </div>
 
-         <?php } ?>
-       </div>
-     </div>
-     <div class="row">
-       <!-- Footer -->
-       <footer class="footer col-md-12" style="position:relative">
-         <div class="row">
-           <div class="foot-section col-md-4" id="contacto">
-             <span>+52 777 123 45 67</span>
-             <br>
-             <span>example@domain.com.mx</span>
-             <br>
-             <span>postal code: 63866</span>
-             <br>
-           </div>
-           <div class="foot-section col-md-4" id="copyright">
-             <span>2016 Cirkuits all rights reserved &copy;</span>
-             <br>
-           </div>
-           <div class="foot-section social" id="social-1">
-             <a href="http://www.twitter.com" target="_blank"><span style="font-size:28pt; color:#FFF;"><i class="fab fa-twitter-square"></i></span></a>
-             <a href="http://www.facebook.com" target="_blank"><span style="font-size:28pt; color:#FFF;"><i class="fab fa-facebook"></i></span></a>
-             <a href="http://www.youtube.com" target="_blank"><span style="font-size:28pt; color:#FFF;"><i class="fab fa-youtube"></i></span></a>
-             <a href="http://www.instagram.com" target="_blank"><span style="font-size:28pt; color:#FFF;"><i class="fab fa-instagram"></i></span></a>
-           </div>
-         </div>
-       </footer>
-     </div>
-   </div>
-   <script>
+        <div class="leaderboard-item">
+          <div class="item-picture">
+            <img src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="avatar.png" class="img img-rounded" width="128px">
+          </div>
+          <div class="item-data">
+            <div class="item-name">
+              <?= $_SESSION["user"]["nombre_usuario"] ?>
+            </div>
+            <div class="item-hscore">
+              100
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+  <script src="../js/games/three.js" charset="utf-8"></script>
+  <script type="text/javascript">    
+      $(document).ready( function(){
+        $('#logModal').modal('show');
+        responsiveEngine();
+      });
 
-   </script>
- </body>
- </html>
+    var commonResponsive = swidth => {
+          $('.separator').css('visibility', 'hidden');    
+    }
+
+    var tabletResponsive = (swidth) =>{
+      commonResponsive(swidth);
+      $('#toggle').removeClass('col-md-1');
+      $('#logoContainer').addClass('col-md-6');
+      $('#logoContainer').removeClass('col-md-4');
+      $('.img_logo').css('width', '256');
+      $('#avatarContainer').removeClass('col-md-3');
+      $('#avatarContainer').addClass('col-md-4');
+    }
+    var mobileResponsive = (swidth) =>{
+        commonResponsive(swidth);
+        $('.img_logo').css('width', '200');
+        $('.img_logo').css('height', '80');
+        $('#logoContainer').removeClass('col-md-6');
+        $("#logoContainer > a").css('margin-left','0%');
+        $('.contenido-dashboard').css('height', 'fit-content');
+        $('#toggle').removeClass('col-md-1');
+        $('#avatarContainer').remove();
+        $('#supportFooter').css('width','100%');
+        $('#contactoFooter').css('width', '100%');
+        $('#contactoFooter').css('margin-left','10%');
+        $('#supportFooter').css('text-align','justify');
+        $('#supportFooter').css('margin-left','10%');
+        $('#supportFooter').css('margin-top','5%');
+        $('#resavatar').css('visibility','visible');
+        $('#reslogout').css('visibility','visible');                                     
+        $('#menu').css('text-align', 'justify');
+        $('#resavatar').append('<a class="nav-link" href="<?=$url;?>profile"> <img src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="avatar.png" class="img img-rounded" width="64px" style="top:-10px" /> </a>');
+        $('#reslogout').append('<a class="nav-link" href="<?=$url;?>exit.php"><span class="badge badge-danger">Log out</span></a> ');
+        $('#resavatar').removeClass('hidden');
+        $('#reslogout').removeClass('hidden');
+        $('.text-center > h1').css('font-size', '1.5em');
+        $('.text-center > h1 > span').css('font-size', '1em');        
+      }
+      var responsiveEngine = () => {
+      var SCREEN_WIDTH = $(window).width();
+      var SCREEN_HEIGHT = $(window).height();
+      /** /////////////// RESPONSIVE ////////////// */
+      if(SCREEN_WIDTH > 1024){
+        $("#reslogout > a").remove();
+        $("#resavatar > a").remove();
+      }
+      if(SCREEN_WIDTH <= 1404){
+        $("#logoContainer > a").css('margin-left','45%');
+      } else if(SCREEN_WIDTH >= 1404){
+        $("#logoContainer > a").css('margin-left','54%');
+      }      
+      /**//////////////// TABLET (800 - 425) ////////// */
+      if(SCREEN_WIDTH <= 800 && SCREEN_WIDTH > 425){
+        tabletResponsive(SCREEN_WIDTH);
+      }
+      /**//////////////// MOBILE (425 - 325) ////////// */
+      if(SCREEN_WIDTH <= 425){
+        mobileResponsive(SCREEN_WIDTH);
+      }
+    }   
+  </script>
+</body>
+</html>
