5a5e6163132539c3f3ecb8db2b5e7454f61de540
MFuentes1992 <markfuentes2991@gmail.com>
2020-07-31 17:50:36 -0500

    New dashboard desing

--- dashboard/index.php
+++ dashboard/index.php
@@ -51,110 +51,71 @@
 
   </head>
   <body>
-    <div class="pos-f-t">
-      <nav class="navbar sticky-top navbar-dark bg-dark">
-        <div class="col-md-1" id="toggle">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+    <div class="aside-menu">
+      <div id="aside-logo-container">
+        <img src="../img/bw_logo.png" alt="Cirkuits logo">
+      </div>
+      <div id="menu-container">
+        <div id="user-avatar-container">
+          <img id="avatar-usuario" src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="<?= $_SESSION["user"]["avatar_usuario"] ?>" />
+          <p id="user-name"><strong><?= $_SESSION["user"]["nombre_usuario"] ?></strong>&nbsp;<strong><?= $_SESSION["user"]["apellido_usuario"] ?></strong></p>
         </div>
-        <div class="col-md-6" id="logoContainer">
-          <a href="<?=$url;?>dashboard" ><img class="img_logo" src="<?=$url; ?>img/bw_logo.png" alt="cirkuits logo"/></a>
-        </div>
-        <div class="col-md-3" id="avatarContainer">
-          <div class="line" style="margin-top:20px;">
-            <a class="nav-link" href="<?=$url;?>exit.php"><span class="badge badge-danger">Log out</span></a>    
-          </div>
-          <div class="line">
-            <a class="nav-link" href="<?=$url;?>profile"> <img src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="avatar.png" class="img img-rounded" width="64px" style="top:-10px" /> </a>
-          </div>
-        </div>
-      </nav>
-      <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg-dark" style="padding-left:1.5rem;">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link white" href="<?=$url;?>dashboard"><i class="fas fa-window-maximize"></i>&nbsp;Dashboard</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link white" href="<?=$url;?>payment/"><i class="far fa-credit-card"></i>&nbsp;Payment and Subscription</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link white" href="<?=$url;?>videos/"><i class="fas fa-film"></i>&nbsp;Videos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link white" href="<?=$url;?>videogames/"><i class="fas fa-gamepad"></i>&nbsp;Videogames</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link white" href="<?=$url;?>materials/"><i class="far fa-file-pdf"></i>&nbsp;Materials</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link white" href="<?=$url;?>support/"><i class="far fa-comments"></i>&nbsp;support</a>
-            </li>
-            <li id="resavatar" class="hidden">              
-            </li>
-            <li id='reslogout' class="hidden">              
-            </li>
-          </ul>
+        <div class="menu-item">
+            <a href="" class="c-badge-primary margin-5">Setings</a>
+            <a href="" class="c-badge-primary margin-5">Help</a>
+            <a href="" class="c-badge-red margin-5">logout</a>
         </div>
       </div>
     </div>
-
-
-
     <div class="container-fluid-dashboard">
-
       <!-- Modal for displaying message -->
-      <?php if($payment == 1) { ?>
-      <div class="modal fade bs-example-modal-sm" id="logModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-       <div class="modal-dialog">
-         <div class="modal-content">
-           <div class="modal-header">
-             <h4 class="modal-title txt-color-darkgray">Payment</h4>
-             <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="ico-close-add"><span aria-hidden="true">&times;</span></button>
-           </div>
-           <div class="modal-body txt-color-darkgray">
-             <span style="font-size: 3em; margin-right:0.5em;"> <i class="fas fa-exclamation-triangle"></i> </span>Please add a Credit Card to proceed.
-           </div>
-           <div class="modal-footer">
-             <button type="button" class="btn btn-primary" data-dismiss="modal" id="btn-close-add">Close</button>
-           </div>
-         </div>
-       </div>
-     </div>
-     <?php } ?>
-
-
-
-      <div class="row">
+        <?php if($payment == 1) { ?>
+        <div class="modal fade bs-example-modal-sm" id="logModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title txt-color-darkgray">Payment</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="ico-close-add"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body txt-color-darkgray">
+              <span style="font-size: 3em; margin-right:0.5em;"> <i class="fas fa-exclamation-triangle"></i> </span>Please add a Credit Card to proceed.
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-dismiss="modal" id="btn-close-add">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php } ?>
+      <div>
         <div class="contenido-dashboard">
           <div class="text-center" style="margin-bottom:2%;">
             <br>
             <br>
-            <h1>Welcome<span style="margin-left:0.5%;" class="welcome-name" ><?php echo $_SESSION["user"]["alter_usuario"] ?></span> </h1>
+            <h1>Welcome:<span style="margin-left:0.5%;" class="welcome-name" ><?php echo $_SESSION["user"]["alter_usuario"] ?></span> </h1>
           </div>
 
           <div class="container-fluid">
-            <div class="row" id="menu">
-              <div class="menu-item col">
+            <div id="dash-menu">
+              <div class="menu-item-hex">
                 <a class="label txt-color-darkgray link-static" href="#">
                   <span class="dash-ico" style="font-size: 3em;"><i class="fas fa-film"></i></span> <br>
                   <span class="">Videos</span>
                 </a>
               </div>
-              <div class="menu-item col">
+              <div class="menu-item-hex">
                 <a class="label txt-color-darkgray link-static" href="<?=$url;?>videogames">
                   <span class="dash-ico" style="font-size: 3em;"><i class="fas fa-gamepad"></i></span> <br>
                   <span class="">Videogames</span>
                 </a>
               </div>
-              <div class="menu-item col">
+              <div class="menu-item-hex">
                 <a class="label txt-color-darkgray link-static" href="#">
                   <span class="dash-ico" style="font-size: 3em;"><i class="far fa-file-pdf"></i></span> <br>
                   <span class="">Materials</span>
                 </a>
               </div>
-              <div class="menu-item col">
+              <div class="menu-item-hex">
                 <a class="label txt-color-darkgray link-static" href="#">
                   <span class="dash-ico" style="font-size: 3em;"><i class="far fa-comments"></i></span> <br>
                   <span class="">support</span>
@@ -169,69 +130,8 @@
     <script>
       $(document).ready( function(){
         $('#logModal').modal('show');
-        responsiveEngine();
         console.log($(window).width());
       });
-
-    var commonResponsive = swidth => {
-          $('.separator').css('visibility', 'hidden');    
-    }
-
-    var tabletResponsive = (swidth) =>{
-      commonResponsive(swidth);
-      $('#toggle').removeClass('col-md-1');
-      $('#logoContainer').addClass('col-md-6');
-      $('#logoContainer').removeClass('col-md-4');
-      $('.img_logo').css('width', '256');
-      $('#avatarContainer').removeClass('col-md-3');
-      $('#avatarContainer').addClass('col-md-4');
-    }
-    var mobileResponsive = (swidth) =>{
-        commonResponsive(swidth);
-        $('.img_logo').css('width', '200');
-        $('.img_logo').css('height', '80');
-        $('#logoContainer').removeClass('col-md-6');
-        $("#logoContainer > a").css('margin-left','0%');
-        $('#toggle').removeClass('col-md-1');
-        $('.contenido-dashboard').css('height', 'fit-content');
-        $('#avatarContainer').remove();
-        $('#supportFooter').css('width','100%');
-        $('#contactoFooter').css('width', '100%');
-        $('#contactoFooter').css('margin-left','10%');
-        $('#supportFooter').css('text-align','justify');
-        $('#supportFooter').css('margin-left','10%');
-        $('#supportFooter').css('margin-top','5%');
-        $('#resavatar').append('<a class="nav-link" href="<?=$url;?>profile"> <img src="<?=$url;?>img/avatars/<?= $_SESSION["user"]["avatar_usuario"] ?>.png" alt="avatar.png" class="img img-rounded" width="64px" style="top:-10px" /> </a>');
-        $('#reslogout').append('<a class="nav-link" href="<?=$url;?>exit.php"><span class="badge badge-danger">Log out</span></a> ');
-        $('#resavatar').removeClass('hidden');
-        $('#reslogout').removeClass('hidden');
-        $('#menu').css('text-align', 'justify');
-        $('.text-center > h1').css('font-size', '1.5em');
-        $('.text-center > h1 > span').css('font-size', '1em');
-      }
-      var responsiveEngine = () => {
-      var SCREEN_WIDTH = $(window).width();
-      var SCREEN_HEIGHT = $(window).height();
-      /** /////////////// RESPONSIVE ////////////// */
-      if(SCREEN_WIDTH > 1024){
-        $("#reslogout > a").remove();
-        $("#resavatar > a").remove();
-      }
-      if(SCREEN_WIDTH <= 1404){
-        $("#logoContainer > a").css('margin-left','45%');
-      }else if(SCREEN_WIDTH => 1404){
-        $("#logoContainer > a").css('margin-left','60%');
-      }
-
-      /**//////////////// TABLET (800 - 425) ////////// */
-      if(SCREEN_WIDTH <= 800 && SCREEN_WIDTH > 425){
-        tabletResponsive(SCREEN_WIDTH);
-      }
-      /**//////////////// MOBILE (425 - 325) ////////// */
-      if(SCREEN_WIDTH <= 425){
-        mobileResponsive(SCREEN_WIDTH);
-      }
-    }
     </script>
   </body>
   </html>
